55da175d5d624a34de0c40efd2ff59b7cfd2713d
黄雨桐 <hyt@huangyutongdeMacBook-Pro.local>
2019-01-24 18:47:41 -0500

    Added a comment

--- parser/Cache.java
+++ parser/Cache.java
@@ -7,6 +7,7 @@ class Cache<T, V> {
 
     private Map<T, V> cache;
 
+    //The description is on the assignment doc is really confusing on this one. Be sure to check back later.
     V get(T key, Function<? super T, ? extends V> constructor) {
         if (key.equals(null) || constructor.equals(null)) {
             throw new NullPointerException();
