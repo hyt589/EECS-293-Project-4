2644142b05ee50fbb91f70df798efccccfef0939
黄雨桐 <hyt@huangyutongdeMacBook-Pro.local>
2019-01-24 15:53:05 -0500

    Added the barebone structure of Cache class

--- parser/Cache.java
+++ parser/Cache.java
@@ -1,1 +1,17 @@
 package parser;
+
+import java.util.*;
+
+final class Cache<T,V> {
+
+    private Map<T, V> cache;
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    V get(T key, Function<? super T, ? extends V> constructor){
+        
+    }
+
+}
