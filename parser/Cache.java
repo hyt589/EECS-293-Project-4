0e0fcb02935b631ec35e8bb779c37cce991ec95f
黄雨桐 <hyt@huangyutongdeMacBook-Pro.local>
2019-01-24 18:45:46 -0500

    First implementation of get method

--- parser/Cache.java
+++ parser/Cache.java
@@ -1,17 +1,21 @@
 package parser;
 
-import java.util.*;
+import java.util.Map;
+import java.util.function.Function;
 
-final class Cache<T,V> {
+class Cache<T, V> {
 
     private Map<T, V> cache;
 
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
+    V get(T key, Function<? super T, ? extends V> constructor) {
+        if (key.equals(null) || constructor.equals(null)) {
+            throw new NullPointerException();
+        }
 
-    V get(T key, Function<? super T, ? extends V> constructor){
-        
-    }
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
 
+        return constructor.apply(key);
+    }
 }
