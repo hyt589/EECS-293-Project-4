b2aa200e24ba60eb8558884ebc5e06becbd5b282
黄雨桐 <hyt@huangyutongdeMacBook-Pro.local>
2019-01-24 19:55:03 -0500

    Initialized Cache to have an empty map; Added methods to Variable class

--- parser/Cache.java
+++ parser/Cache.java
@@ -1,11 +1,12 @@
 package parser;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 
 class Cache<T, V> {
 
-    private Map<T, V> cache;
+    private Map<T, V> cache = Collections.emptyMap();
 
     //The description is on the assignment doc is really confusing on this one. Be sure to check back later.
     V get(T key, Function<? super T, ? extends V> constructor) {
--- parser/Variable.java
+++ parser/Variable.java
@@ -4,8 +4,28 @@ public final class Variable extends AbstractToken implements Token {
 
     private TerminalSymbol type = TerminalSymbol.VARIABLE;
 
+    private final String representation;
+
+    private static Cache<String, Variable> cache;
+
+    private Variable(String representation) {
+        this.representation = representation;
+    }
+
     public final TerminalSymbol getType() {
         return this.type;
     }
 
+    public final String getRepresentation() {
+        return representation;
+    }
+
+    public static final Variable build(String representation) {
+        return cache.get(representation, Variable::new);
+    }
+
+    @Override
+    public String toString() {
+        return representation;
+    }
 }
